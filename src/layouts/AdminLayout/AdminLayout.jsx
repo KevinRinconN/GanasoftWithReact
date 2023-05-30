0f05e86d567a33e72ebbf66fa6411d16401c7af3
Kevin Rincon <estebanrincon0926@gmail.com>
2023-05-29 21:56:32 -0500

    se añadio

--- src/layouts/AdminLayout/AdminLayout.jsx
+++ src/layouts/AdminLayout/AdminLayout.jsx
@@ -8,7 +8,7 @@ export const AdminLayout = ({children}) => {
    
     const {auth} = useAuth();
 
-    if (auth) return <LoginAdmin/>
+    if (!auth) return <LoginAdmin/>
     
     return (
         <>  
